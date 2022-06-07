b3fc9fdeb72b8a7b5648c6eb0ec5ae47f66dac7e
rajanbramhankar <99976468+rajanbramhankar@users.noreply.github.com>
2022-06-07 17:39:31 +0530

    Update Blockhome.js

--- src/Blocks/Blockhome.js
+++ src/Blocks/Blockhome.js
@@ -1,24 +1,23 @@
 import React from 'react'
+import '../component/Bollywood.css'
 
-
-const Blockhome = (props) => {
-    const { img, description, title, category }=props;
+const Blocktoppost = (props) => {
+    const { img,  title }=props;
   return (
-  
          <>
-         
-         <div className='block_home_middle_container' >
-              <img className='block_home_img_middle' src={img} alt='not found' />
-             <div className='block_home_middle'>
-             <h2 className='block_title_home_middle' >{title}</h2>
-              <p className='block_description_home_middle' >{description}</p>
-              <p className='category_home_middle' ><b>{category}</b></p>
-             </div>
-          </div>
         
+          <div className='block_home' >
+           
+           <div className='top_post'>
+           <img className='block_top_post_img' src={img} alt='not found' />
+             <h4 className='block_top_post_title' >{title}</h4>
+           </div>
+          
+            
+          </div>
           
           </>
   )
 }
 
-export default Blockhome
+export default Blocktoppost
