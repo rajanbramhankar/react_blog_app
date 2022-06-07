55b163425e3436c2f097465f9589b424a44aaa1a
rajanbramhankar <99976468+rajanbramhankar@users.noreply.github.com>
2022-06-07 17:44:37 +0530

    Update Blocktoppost.js

--- src/Blocks/Blocktoppost.js
+++ src/Blocks/Blocktoppost.js
@@ -9,7 +9,7 @@ const Blocktoppost = (props) => {
           <div className='block_home' >
            
            <div className='top_post'>
-           <img className='block_top_post_img' src={img} alt='no image found' />
+           <img className='block_top_post_img' src={img} alt='not found' />
              <h4 className='block_top_post_title' >{title}</h4>
            </div>
           
